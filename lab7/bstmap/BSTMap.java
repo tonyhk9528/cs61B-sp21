51971a9da3e7f2cbdd0eac7c90e99a4c86ae5904
tonyhk9528 <tonytsanghinfung@gmail.com>
2025-12-27 10:54:44 +0000

    Done with lab7 v2

--- lab7/bstmap/BSTMap.java
+++ lab7/bstmap/BSTMap.java
@@ -21,7 +21,7 @@ public class BSTMap<K extends Comparable<K>, V>  implements Map61B<K, V>  {
         }
     }
 
-    BSTMap () {
+    public BSTMap () {
         this.root = null;
         size = 1;
     }
